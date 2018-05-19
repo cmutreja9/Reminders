6aa2256b7820222ac78a27b1818c3493940afe13
Chinky Mutreja <z5128456@w-c02vv00ch7vg.ad.unsw.edu.au>
2018-05-20 01:46:46 +1000

    first commit

---
+++ TheReminder/RoundButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundButton.swift
+//  TheReminder
+//
+//  Created by Chinky Mutreja on 19/5/18.
+//  Copyright © 2018 Chinky Mutreja. All rights reserved.
+//
+
+import UIKit
+
+class RoundButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
